f85b36a8ab255afd8157615a88753b0c4ca6ed57
SAKA7 <ihor.sakaylyuk@gmail.com>
2016-03-14 19:49:58 +0200

    Initial commit

---
+++ src/com/sqlibri/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.sqlibri.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class MainController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+	}
+
+}
