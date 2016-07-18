8bf09997fba054377391df52146271aa2ac5bae0
Saka7 <ihorsakaylyuk@gmail.com>
2016-07-18 13:57:07 +0300

    Refactoring App file

--- src/com/sqlibri/App.java
+++ src/com/sqlibri/App.java
@@ -1,12 +1,14 @@
 package com.sqlibri;
 
+import java.net.URL;
+
 import com.sqlibri.presenter.AppPersenter;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -15,24 +17,30 @@ import javafx.stage.Stage;
 public class App extends Application {
 
 	// Application Icon
-	private final Image rootIcon = new Image(getClass()
-			.getResource("resources/image/SQLibri-icon.png").toExternalForm());
+	private final Image ROOT_ICON = new Image(
+			getClass().getResource("resources/image/SQLibri-icon.png").toExternalForm());
+	
+	// Application title
+	private final String TITLE = "SQLibri";
+	
+	// Main Layout
+	private final URL MAIN_LAYOUT = getClass()
+			.getResource("resources/layout/main-layout.fxml");
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass()
-					.getResource("resources/layout/main-layout.fxml"));
-			BorderPane root = (BorderPane) loader.load();
-			
+			FXMLLoader loader = new FXMLLoader(MAIN_LAYOUT);
+			Parent root = loader.load();
+
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
-			
+
 			AppPersenter controller = (AppPersenter) loader.getController();
 			controller.init(primaryStage);
-			
-			primaryStage.setTitle("sqlibri");
-			primaryStage.getIcons().add(rootIcon);
+
+			primaryStage.setTitle(TITLE);
+			primaryStage.getIcons().add(ROOT_ICON);
 			primaryStage.setMaximized(true);
 			primaryStage.show();
 		} catch (Exception e) {
